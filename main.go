909de410a89247803eb1d5b302aee19be50112fc
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-14 16:49:54 +0200

    feat: exit cli command

--- main.go
+++ main.go
@@ -2,28 +2,58 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func cleanInput(text string) []string {
 	return strings.Fields(text)
 }
 
+func commandExit() error {
+	fmt.Print("Closing the Pokedex... Goodbye!\n")
+	os.Exit(0)
+	return errors.New("exit failed")
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		userInput := scanner.Text()
 		cleanedInput := cleanInput(userInput)
-		var command string
+		var userCommand string
 		if len(cleanedInput) == 0 {
-			command = ""
+			userCommand = ""
 		} else {
-			command = strings.ToLower(cleanedInput[0])
+			userCommand = strings.ToLower(cleanedInput[0])
 		}
-		fmt.Printf("Your command was: %v\n", command)
+		command, ok := commands[userCommand]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Printf("error executing command: %v", err)
+			}
+		} else {
+			fmt.Printf("Unknown command\n")
+		}
 	}
 }
