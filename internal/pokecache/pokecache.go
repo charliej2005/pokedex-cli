a8a4381bcbb0ebfba890e450d1d36da1e9d3a852
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-15 19:33:10 +0200

    feat: cache functions

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -20,5 +20,39 @@ func NewCache(interval time.Duration) Cache {
 		cache: make(map[string]cacheEntry),
 		mux:   &sync.RWMutex{},
 	}
+	c.reapLoop(interval)
 	return c
+}
+
+func (c Cache) Add(key string, value []byte) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	c.cache[key] = cacheEntry{
+		createdAt: time.Now().UTC(),
+		val:       value,
+	}
+}
+
+func (c Cache) Get(key string) ([]byte, bool) {
+	c.mux.RLock()
+	defer c.mux.RUnlock()
+	cEntry, ok := c.cache[key]
+	return cEntry.val, ok
+}
+
+func (c Cache) reapLoop(interval time.Duration) {
+	ticker := time.NewTicker(interval)
+	for range ticker.C {
+		c.reap(time.Now().UTC(), interval)
+	}
+}
+
+func (c Cache) reap(now time.Time, last time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+	for key, val := range c.cache {
+		if val.createdAt.Before(now.Add(-last)) {
+			delete(c.cache, key)
+		}
+	}
 }
