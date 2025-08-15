7e1acc17b77bfcd2d84eae9eab531535d4e7a2ab
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-15 19:13:29 +0200

    feat: cache struct

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,24 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	cache map[string]cacheEntry
+	mux   *sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) Cache {
+	c := Cache{
+		cache: make(map[string]cacheEntry),
+		mux:   &sync.Mutex{},
+	}
+	return c
+}
