f7fb976932d589b644237bc2a469e053a6cc8723
CharlieJ2005 <163343248+CharlieJ2005@users.noreply.github.com>
2025-08-15 18:08:50 +0200

    feat: internal api logic

---
+++ internal/location_list.go
@@ -0,0 +1,33 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
+	url := baseURL + "/location-area"
+	if pageURL != nil {
+		url = *pageURL
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return RespShallowLocations{}, err
+	}
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return RespShallowLocations{}, err
+	}
+
+	locationRes := RespShallowLocations{}
+	err = json.Unmarshal(data, &locationRes)
+	if err != nil {
+		return RespShallowLocations{}, err
+	}
+
+	return locationRes, nil
+}
